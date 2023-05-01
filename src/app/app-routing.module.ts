53ceb96381beffcce7a41cb68c52af0fdb415299
saucesar <sau.cesarlima2013@gmail.com>
2023-05-01 16:04:13 -0300

    Ajuste e redirect para rota home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component'
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomePageComponent},
   { path: 'login', component: LoginComponent },
 ];
 
