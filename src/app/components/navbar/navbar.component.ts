aaea93bd96ba9dc5c4845a5a03f88e89e57b8d72
saucesar <sau.cesarlima2013@gmail.com>
2023-05-05 00:02:43 -0300

    Ajuste para exibir menu de usuário

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
+import { EventService } from 'src/app/services/event.service';
 
 
 @Component({
@@ -16,22 +17,45 @@ export class NavbarComponent implements OnInit {
   constructor(
     private router: Router,
     private toast: ToastrService,
-    private authService: AuthService
+    private authService: AuthService,
+    private eventService: EventService,
   )
   {}
 
   ngOnInit(): void {
     this.user = this.authService.getCurrentUser()
-    this.authService.isLoggedIn.subscribe(
-      (loggedIn) => {
-        this.isLoggedIn = loggedIn
+    this.isLoggedIn = this.authService.isLoggedIn
+
+    this.eventService.loginEvent.subscribe(
+      () => {
+        this.isLoggedIn = true
+        this.user = this.authService.getCurrentUser()
       }
     )
+
+    this.eventService.logoutEvent.subscribe(
+      () => {
+        this.isLoggedIn = false
+        this.user = null
+      }
+    )
+  }
+
+  get settingsIcon() {
+    return document.getElementById('settings-icon')
   }
 
   public logout() {
     this.authService.logout()
     this.router.navigate(['/home'])
     this.toast.success('Sessão encerrada', 'Sucesso')
+  }
+
+  public mouseOver() {
+    this.settingsIcon?.classList.add('fa-spin')
+  }
+
+  public mouseLeave() {
+    this.settingsIcon?.classList.remove('fa-spin')
   }
 }
