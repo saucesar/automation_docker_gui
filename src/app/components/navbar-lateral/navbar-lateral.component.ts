9db65d6aa60f8c76b9faa4aa3a46d553f5898dd1
saucesar <sau.cesarlima2013@gmail.com>
2023-05-08 09:26:49 -0300

    Ajustes para animação dos menus

--- src/app/components/navbar-lateral/navbar-lateral.component.ts
+++ src/app/components/navbar-lateral/navbar-lateral.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-lateral',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar-lateral.component.scss']
 })
 export class NavbarLateralComponent {
+  constructor(private renderer: Renderer2)
+  {}
 
+  public mouseOver(elementId: string) {
+    const element = this.renderer.selectRootElement(`#${elementId}`)
+    console.log(element.classList)
+    element.classList.add('fa-beat')
+  }
+
+  public mouseLeave(elementId: string) {
+    const element = this.renderer.selectRootElement(`#${elementId}`)
+    console.log(element.classList)
+    element.classList.remove('fa-beat')
+  }
 }
